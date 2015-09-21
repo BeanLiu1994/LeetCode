85185e7040372b9b409bf534047fdb99f4d117d1
BeanLiu <njueebeanliu@hotmail.com>
2015-09-21 15:34:11 +0800

    twoSum finished

--- twoSum/twoSum/twoSum.cpp
+++ twoSum/twoSum/twoSum.cpp
@@ -6,23 +6,32 @@ using namespace std;
 class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
-		sort(nums.begin(), nums.end());
+		vector<int> nums_workspace = nums;
+		sort(nums_workspace.begin(), nums_workspace.end());
 		int id1 = 1, id2 = nums.size();
 		int sum;
 		do{
-			sum = nums[id1 - 1] + nums[id2 - 1];
+			sum = nums_workspace[id1 - 1] + nums_workspace[id2 - 1];
 			if (sum > target) --id2;
 			else if (sum < target) ++id1;
 			else break;
 		} while (id2 > id1);
-		id1_origin = find(nums.begin(), nums.end(), ) - nums.begin();
-		return vector<int>({ id1, id2 });
+		int id1_origin = find(nums.begin(), nums.end(), nums_workspace[id1 - 1]) - nums.begin() + 1;
+		int id2_origin = find(nums.begin(), nums.end(), nums_workspace[id2 - 1]) - nums.begin() + 1;
+		if (id2_origin == id1_origin) {
+			id2_origin = find(nums.begin() + id1_origin, nums.end(), nums_workspace[id2 - 1]) - nums.begin() + 1;
+		}
+		vector<int> result({ id1_origin, id2_origin });
+		sort(result.begin(), result.end());
+		return result;
 	}
 };
 
 int main()
 {
+	vector<int> input({0,4,3,0});
 	Solution a;
+
 	getchar();
 	return 0;
 }
